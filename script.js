a48b75543659d94ab6ecaa8efa9afb207ea5fda4
Grace <gracekolago21@gmail.com>
2024-09-12 04:07:51 -0700

    belajar html,css,dan js

---
+++ script.js
@@ -0,0 +1,18 @@
+var x =10;
+var y =10;
+var z = x * y;
+
+
+function perkalian(x,y){
+    return x * y;
+}
+perkalian (4,4);
+perkalian (6,3)
+
+
+const btn = document.getElementById("btn");
+const tampilan = document.getElementById("tampilan");
+
+ btn.onclick = function () {
+    tampilan.innerHTML = "Welcome Grace" ;
+ }
